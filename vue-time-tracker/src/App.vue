006b8d19d9c0eae55d6513d6feae0191351ce065
vino24 <liugq19931023@gmail.com>
2016-07-15 11:27:31 +0800

    init

---
+++ vue-time-tracker/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="wrapper">
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <i class="glyphicon glyphicon-time"></i>
+          计划表
+        </a>
+        <ul class="nav navbar-nav">
+          <li><a v-link="'/home'">首页</a></li>
+          <li><a v-link="'/time-entries'">计划列表</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="col-sm-3">
+          <sidebar :time="totalTime"></sidebar>
+      </div>
+      <div class="col-sm-9">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import Sidebar from './components/Sidebar.vue'
+  export default {
+    components: { 'sidebar': Sidebar },
+    data () {
+      return {
+        totalTime: 1.5
+      }
+    },
+    events: {
+      timeUpdate (timeEntry) {
+        this.totalTime += parseFloat(timeEntry.totalTime)
+      },
+      deleteTime (timeEntry) {
+        this.totalTime -= parseFloat(timeEntry.totalTime)
+      }
+    }
+  }
+</script>
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ vue-time-tracker/src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import App from './App'
+import Home from './components/Home.vue'
+import TimeEntries from './components/TimeEntries.vue'
+import LogTime from './components/LogTime.vue'
+
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+const router = new VueRouter()
+
+router.map({
+	'/Home': {
+		component: Home
+	},
+	'/time-entries': {
+		component: TimeEntries,
+		subRoutes: {
+			'/log-time': {
+				component: LogTime
+			}
+		}
+	}
+})
+router.redirect({
+	'*': '/Home'
+})
+router.start(App, '#app')
+/* eslint-disable no-new */
+new Vue({
+  el: 'body',
+  components: { App }
+})
